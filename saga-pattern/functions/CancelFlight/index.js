10e3cab249e69baa516625bc9373e20760d6acd1
eelayoubi <ezzeddine.el.ayoubi@gmail.com>
2022-10-14 09:47:21 +0200

    feat: add cancel lambda functions

---
+++ saga-pattern/functions/CancelFlight/index.js
@@ -0,0 +1,27 @@
+const AWS = require("aws-sdk")
+const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+const TABLE_NAME = process.env.TABLE_NAME
+
+exports.handler = async (event) => {
+    console.log("Event: ", event)
+
+    var params = {
+        TableName: TABLE_NAME,
+        Key: {
+            'id' : {S: event.confirmation_id}
+        }
+    };
+
+    try {
+          await ddb.deleteItem(params).promise();
+          console.log('Success')
+          return {
+            statusCode: 201,
+            body: "Cancel Flight Success",
+          };
+        } catch (error) {
+            console.log('Error: ', error)
+            throw new Error("ServerError")
+        }
+};
