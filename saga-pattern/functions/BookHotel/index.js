4421ca687c72b4ea8d61e78a616ea0bfd69e225c
eelayoubi <ezzeddine.el.ayoubi@gmail.com>
2022-10-14 09:02:45 +0200

    feat: add lambda functions

---
+++ saga-pattern/functions/BookHotel/index.js
@@ -0,0 +1,48 @@
+const AWS = require("aws-sdk")
+const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+const TABLE_NAME = process.env.TABLE_NAME
+
+class BookHotelError extends Error {
+  constructor(message) {
+    super(message)
+    this.name = "BookingHotelError"
+  }
+}
+
+exports.handler = async (event) => {
+    console.log('Event: ', event)
+    const {
+      confirmation_id,
+      checkin_date,
+      checkout_date
+    } = event
+
+    var params = {
+        TableName: TABLE_NAME,
+        Item: {
+            'id' : {S: confirmation_id},
+            'checkin_date' : {S: checkin_date},
+            'checkout_date': {S: checkout_date}
+        }
+    };
+
+    try {
+          await ddb.putItem(params).promise();
+          console.log('Success')
+
+        } catch (error) {
+          console.log('Error: ', error)
+          throw new Error("Unexpected Error")
+        }
+        
+        if(confirmation_id.startsWith("11")){
+          throw new BookHotelError("Expected Error")
+        }
+        
+        return {
+          confirmation_id,
+          checkin_date,
+          checkout_date
+        };
+};
---
+++ saga-pattern/functions/BookRental/index.js
@@ -0,0 +1,47 @@
+const AWS = require("aws-sdk")
+const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+const TABLE_NAME = process.env.TABLE_NAME
+
+class BookRentalError extends Error {
+  constructor(message) {
+    super(message)
+    this.name = "BookRentalError"
+  }
+}
+
+exports.handler = async (event) => {
+    const  {
+          confirmation_id,
+          checkin_date,
+          checkout_date
+        } = event
+        
+    var params = {
+        TableName: TABLE_NAME,
+        Item: {
+            'id' : {S: confirmation_id},
+            'checkin_date' : {S: checkin_date},
+            'checkout_date': {S: checkout_date}
+        }
+    };
+
+    try {
+          await ddb.putItem(params).promise();
+          console.log('Success')
+
+        } catch (error) {
+          console.log('Error: ', error)
+          throw new Error("Unexpected Error")
+        }
+        
+        if(confirmation_id.startsWith("33")){
+          throw new BookRentalError("Expected Error")
+        }
+        
+        return {
+          confirmation_id,
+          checkin_date,
+          checkout_date
+        };
+};
